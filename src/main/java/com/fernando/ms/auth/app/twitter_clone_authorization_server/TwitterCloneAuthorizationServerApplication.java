62f613733cc122f55e26c0b5a52fed3f2b5e0837
Fernando <asialer05@hotmail.com>
2025-01-16 18:18:08 -0500

    proyecto base

---
+++ src/main/java/com/fernando/ms/auth/app/twitter_clone_authorization_server/TwitterCloneAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.fernando.ms.auth.app.twitter_clone_authorization_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterCloneAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterCloneAuthorizationServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernando/ms/auth/app/twitter_clone_authorization_server/TwitterCloneAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernando.ms.auth.app.twitter_clone_authorization_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterCloneAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
